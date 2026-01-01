28c8494d372bc83b8d0822288c6acb5a3956995f
20pedro01 <157173148+20pedro01@users.noreply.github.com>
2026-01-01 06:31:42 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,12 @@ window.addEventListener('load', function () {
   body.classList.remove('view-2D', 'opening');
   body.classList.add('view-3D', 'set-speed');
   universe.className = "scale-stretched set-speed";
+  var audio = document.getElementById("bg-audio");
+  var startBtn = document.getElementById("start-btn");
+  var overlay = document.getElementById("start-overlay");
+
+  startBtn.addEventListener('click', function () {
+    overlay.classList.add('hidden');
+    audio.play();
+  });
 });
